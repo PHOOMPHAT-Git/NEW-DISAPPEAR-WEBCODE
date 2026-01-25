613c7433ad38871e2563ad490c44bd97e3adae93
PHOOMPHAT-Git <phomphat2011@gmail.com>
2026-01-25 19:08:53 +0700

    update

--- src/models/User.js
+++ src/models/User.js
@@ -1,15 +1,16 @@
 const mongoose = require('mongoose');
 
-const UserSchema = new mongoose.Schema(
-    {
-        username: { type: String, required: true, unique: true },
-        email: { type: String, required: true, unique: true },
-        password: { type: String, required: true },
-        settings: {
-            hideEmail: { type: Boolean, default: false }
-        }
+const UserSchema = new mongoose.Schema({
+    username: { type: String, required: true, unique: true, trim: true },
+    email: { type: String, required: true, unique: true, trim: true, lowercase: true },
+    password: { type: String, required: true },
+    token: { type: String, required: true, unique: true },
+    friends: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+    settings: {
+        hideEmail: { type: Boolean, default: true }
     },
-    { timestamps: true }
-);
+    created_at: { type: Date, default: Date.now },
+    updated_at: { type: Date, default: Date.now }
+});
 
 module.exports = mongoose.models.User || mongoose.model('User', UserSchema, 'users');
